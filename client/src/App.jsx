9faef18d02cbcf18ccadf30caeab127ea649eb79
chucker34 <kawakami34@gmail.com>
2022-12-11 16:51:17 +0900

    10-7まで

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,64 @@
-import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
-import "./App.css";
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import FundraiserFactoryContract from './contracts/FundraiserFactory.json'
+import getWeb3 from './getWeb3'
+import './App.css'
+import NewFundraiser from './NewFundraiser'
+import Home from './Home'
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({
+    web3: null,
+    accounts: null,
+    contract: null,
+  })
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3 = await getWeb3()
+        const accounts = await web3.eth.getAccounts()
+        const networkId = await web3.eth.net.getId()
+        const deployedNetwork = FundraiserFactoryContract.networks[networkId]
+        const instance = new web3.eth.Contract(
+          FundraiserFactoryContract.abi,
+          deployedNetwork && deployedNetwork.address
+        )
+        setState({ web3, accounts, contract: instance })
+      } catch (error) {
+        alert(
+          `App.js: Failed to load web3, accounts, or contract.
+           Check console for details.`
+        )
+        console.error(error)
+      }
+    }
+    init()
+  }, [])
+
+  const runExample = async () => {
+    const { accounts, contract } = state
+  }
+
   return (
-    <EthProvider>
-      <div id="App" >
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/new/">New</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/new/" component={NewFundraiser} />
       </div>
-    </EthProvider>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
