34151c33f7891a8dda317eac059ef9095358f47a
chucker34 <kawakami34@gmail.com>
2022-12-15 18:06:50 +0900

    10-21まで

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,34 @@
 import React, { useState, useEffect } from 'react'
+import FundraiserFactoryContract from './contracts/FundraiserFactory.json'
+import Web3 from 'web3'
+import detectEthereumProvider from '@metamask/detect-provider'
 
 const Home = () => {
-  useEffect(() => {}, [])
+  const [contract, setContract] = useState(null)
+  const [accounts, setAccounts] = useState(null)
+
+  useEffect(() => {
+    init()
+  }, [])
+
+  const init = async () => {
+    try {
+      const provider = await detectEthereumProvider()
+      const web3 = new Web3(provider)
+      const networkId = await web3.eth.net.getId()
+      const deployedNetwork = FundraiserFactoryContract.networks[networkId]
+      const accounts = await web3.eth.getAccounts()
+      const instance = new web3.eth.Contract(
+        FundraiserFactoryContract.abi,
+        deployedNetwork && deployedNetwork.address
+      )
+    } catch (error) {
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      )
+      console.error(error)
+    }
+  }
 
   return (
     <div>
